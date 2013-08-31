da37d8bf2f4a2f05f728d00ba4fd005400629873
praaveen <praaveen@ubuntu.ubuntu-domain>
2013-09-01 00:41:27 +0530

    initial sqlite commit

---
+++ gen/com/de/vogella/android/sqlite/first/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.de.vogella.android.sqlite.first;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
